0833e621632321af6082f523dc179f9334b872ce
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-09-27 18:02:10 -0400

    v1.1.0 with SelectionRegions (object and hitbox) connected with a property for context on the RenderService.

--- BassClefStudio.SkiaSharp.Helpers/RenderService.cs
+++ BassClefStudio.SkiaSharp.Helpers/RenderService.cs
@@ -1,6 +1,10 @@
-﻿using SkiaSharp;
+﻿using BassClefStudio.NET.Core;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace BassClefStudio.SkiaSharp.Helpers
@@ -8,22 +12,48 @@ namespace BassClefStudio.SkiaSharp.Helpers
     /// <summary>
     /// Represents a service capable of rendering the graphics representing an object of type <typeparamref name="T"/> onto a <see cref="SKCanvas"/>.
     /// </summary>
-    public abstract class RenderService<T> : IDisposable
+    public abstract class RenderService<T> : Observable, IDisposable
     {
+        /// <summary>
+        /// A collection of <see cref="SKPaint"/>s keyed with <see cref="string"/>s, which are managed by the <see cref="RenderService{T}"/> (e.g. disposal).
+        /// </summary>
         protected Dictionary<string, SKPaint> Paints { get; } = new Dictionary<string, SKPaint>();
 
+        private T attachedContext;
         /// <summary>
-        /// Renders the <typeparamref name="T"/> object onto the given <see cref="SKCanvas"/>.
+        /// The attached <typeparamref name="T"/> context that the <see cref="RenderService{T}"/> renders.
         /// </summary>
-        /// <param name="context">The <typeparamref name="T"/> representing the context of the game/app.</param>
-        /// <param name="canvas">The canvas to render content onto.</param>
-        public abstract void Render(T context, SKCanvas canvas);
+        public T AttachedContext { get => attachedContext; set => Set(ref attachedContext, value); }
 
         /// <summary>
-        /// Gets the <see cref="ISelectable"/> object found at the given SkiaSharp coordinates (for use in selection and click events, etc.)
+        /// Renders the <see cref="AttachedContext"/> onto the given <see cref="SKCanvas"/>.
+        /// </summary>
+        /// <param name="canvas">The canvas to render content onto.</param>
+        public abstract void Render(SKCanvas canvas);
+
+        /// <summary>
+        /// A collection of <see cref="SelectionRegion"/>s which are used to find the selected object in the <see cref="GetSelected(SKPoint)"/> method.
+        /// </summary>
+        protected ObservableCollection<SelectionRegion> SelectionRegions { get; } = new ObservableCollection<SelectionRegion>();
+
+        /// <summary>
+        /// Gets the <see cref="SelectionRegion"/> object found at the given SkiaSharp coordinates for the <see cref="AttachedContext"/> (for use in selection and click events, etc.).
         /// </summary>
         /// <param name="point">The point, in SkiaSharp pixel coordinates, to check for selection.</param>
-        public abstract ISelectable GetSelected(SKPoint point);
+        public SelectionRegion GetSelected(SKPoint point)
+        {
+            return SelectionRegions.LastOrDefault(r => r.SelectionBounds.Contains(point));
+        }
+
+        /// <summary>
+        /// Return the <typeparamref name="TItem"/> model object found at the given SkiaSharp coordinates for the <see cref="AttachedContext"/> (for use in selection and click events, etc.).
+        /// </summary>
+        /// <param name="point">The point, in SkiaSharp pixel coordinates, to check for selection.</param>
+        public TItem GetSelected<TItem>(SKPoint point)
+        {
+            var region = SelectionRegions.LastOrDefault(r => r.SelectionBounds.Contains(point) && r.AttachedObject is TItem);
+            return region != null ? (TItem)region.AttachedObject : default(TItem);
+        }
 
         /// <inheritdoc/>
         public void Dispose()
---
+++ BassClefStudio.SkiaSharp.Helpers/SelectionRegion.cs
@@ -0,0 +1,27 @@
+﻿using BassClefStudio.NET.Core;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BassClefStudio.SkiaSharp.Helpers
+{
+    /// <summary>
+    /// Represents a selectable region as displayed by a <see cref="RenderService{T}"/>.
+    /// </summary>
+    public class SelectionRegion : Observable
+    {
+        private SKRect selectionBounds;
+        /// <summary>
+        /// A <see cref="SKRect"/> representing the bounds of the selection region.
+        /// </summary>
+        public SKRect SelectionBounds { get => selectionBounds; set => Set(ref selectionBounds, value); }
+
+        private object attachedObject;
+        /// <summary>
+        /// The model <see cref="object"/> that is rendered in this <see cref="SelectionRegion"/>.
+        /// </summary>
+        public object AttachedObject { get => attachedObject; set => Set(ref attachedObject, value); }
+    }
+}
