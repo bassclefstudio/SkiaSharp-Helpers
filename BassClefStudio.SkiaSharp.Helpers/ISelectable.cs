0b0e767f095b2dba83063287b7a822e6e713527f
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-09-27 16:58:36 -0400

    Completed documentation and version info.

--- BassClefStudio.SkiaSharp.Helpers/ISelectable.cs
+++ BassClefStudio.SkiaSharp.Helpers/ISelectable.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace BassClefStudio.SkiaSharp.Helpers
 {
+    /// <summary>
+    /// Represents a selectable object displayed by a <see cref="RenderService{T}"/>.
+    /// </summary>
     public interface ISelectable
     {
     }
