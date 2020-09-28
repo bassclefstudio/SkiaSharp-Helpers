78b11a1021fc8fa3378bab00b2bd636bdc0c2fd7
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-09-27 22:55:39 -0400

    Documentation and constructors for SelectionRegion.

--- BassClefStudio.SkiaSharp.Helpers/SelectionRegion.cs
+++ BassClefStudio.SkiaSharp.Helpers/SelectionRegion.cs
@@ -14,7 +14,7 @@ namespace BassClefStudio.SkiaSharp.Helpers
     {
         private SKPoint selectionCenter;
         /// <summary>
-        /// A <see cref="SKPath"/> representing the bounds of the selection region.
+        /// A <see cref="SKPoint"/> representing the center of the selection region.
         /// </summary>
         public SKPoint SelectionCenter { get => selectionCenter; set => Set(ref selectionCenter, value); }
 
@@ -29,5 +29,23 @@ namespace BassClefStudio.SkiaSharp.Helpers
         /// The model <see cref="object"/> that is rendered in this <see cref="SelectionRegion"/>.
         /// </summary>
         public object AttachedObject { get => attachedObject; set => Set(ref attachedObject, value); }
+
+        /// <summary>
+        /// Creates a new <see cref="SelectionRegion"/>.
+        /// </summary>
+        public SelectionRegion() { }
+
+        /// <summary>
+        /// Creates a new <see cref="SelectionRegion"/> for the given <see cref="object"/> at the given position.
+        /// </summary>
+        /// <param name="attached">The model <see cref="object"/> that is rendered in this <see cref="SelectionRegion"/>.</param>
+        /// <param name="point">A <see cref="SKPoint"/> representing the center of the selection region.</param>
+        /// <param name="radiusSquared">The maximum distance away from the <see cref="SelectionCenter"/> a point can be to still be considered selecting this <see cref="SelectionRegion"/>, squared.</param>
+        public SelectionRegion(object attached, SKPoint point, float radiusSquared)
+        {
+            AttachedObject = attached;
+            SelectionCenter = point;
+            RadiusSquared = radiusSquared;
+        }
     }
 }
